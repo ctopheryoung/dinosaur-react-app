69045e120fcd646202659e263a73644c04c92783
Chris Young <ctopheryoung@gmail.com>
2022-11-28 11:08:02 -0800

    setup routing

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import react from 'npm:@vitejs/plugin-react@^2.2.0'
 
 import 'npm:react@^18.2.0'
 import 'npm:react-dom@^18.2.0/client'
+import "npm:react-router-dom@^6.4"
 
 // https://vitejs.dev/config/
 export default defineConfig({
